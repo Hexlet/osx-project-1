16f5fc4a2aee4957b74945d1fb6ebc595a531f1a
Dmitriy Rybalko <rdtft@me.com>
2012-10-30 23:34:44 +0200

    removed reference doc

--- assignment-1/main.m
+++ assignment-1/main.m
@@ -5,9 +5,9 @@
 int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
-        
         Cell *cell1 = [[Cell alloc] init];
         Cell *cell2 = [[Cell alloc] init];
+
         uint hd = [cell1 hammingDistance: cell2];
         
         NSLog(@"hamming distance= %d", hd);
@@ -18,7 +18,7 @@ int main(int argc, const char * argv[]) {
         hd = [cell1 hammingDistance: cell2];
         
         NSLog(@"hamming distance= %d", hd);
-        
     }
+    
     return 0;
 }
