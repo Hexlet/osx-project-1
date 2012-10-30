631a845f7969f1e6ae34ad3f81cb49451d3761c6
Pavel Dmitriev <p.dmitriev@nimble.com>
2012-10-30 18:15:54 +0200

    Initial Commit

---
+++ FunnyBiologist/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FunnyBiologist
+//
+//  Created by Paul Dmitryev on 30.10.12.
+//  Copyright (c) 2012 Paul Dmitryev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
