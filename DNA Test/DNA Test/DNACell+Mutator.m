fcd2162f02c5f38c86e2eaf1640a367ea121ded0
Evgeny Golubev <evgeny@golubev.eu>
2012-10-31 08:44:11 +0300

    v 0.3

---
+++ DNA Test/DNA Test/DNACell+Mutator.m
@@ -0,0 +1,13 @@
+//
+//  DNACell+Mutator.m
+//  DNA Test
+//
+//  Created by Evgeny Golubev on 31.10.12.
+//  Copyright (c) 2012 Evgeny Golubev. All rights reserved.
+//
+
+#import "DNACell+Mutator.h"
+
+@implementation DNACell (Mutator)
+
+@end
