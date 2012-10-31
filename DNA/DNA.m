8b71a669eb62d261f6681f278eb6d4125e53673c
Un filet <suchvel@mail.ru>
2012-10-31 21:39:16 +0000

    FINAL

--- DNA/DNA.m
+++ DNA/DNA.m
@@ -20,34 +20,35 @@
 }
 
 -(void) mutate:(float) percent {
-
+    
 	if ((percent > 100.0f) || (percent < 0.0f))
 		return;
-
-	int capacity = [[self dnaArray] count];
-	for (int i = 0; i < capacity*percent/100.0f; i++) {
-
-		// index -  случайный индекс
-	        int index = arc4random()%capacity;
-		// object - случайный ген
-	        NSString* object = [self differentObjectsAtIndex:index];
+    
+    int capacity = [[self dnaArray] count];	
+    int percentCapacity = capacity*percent/100.0f;
+    
+    NSMutableArray* unique = [[NSMutableArray alloc] initWithCapacity:percentCapacity];
+    
+	
+	for (int i = 0; i < percentCapacity; i++) {
         
-        	[[self dnaArray] replaceObjectAtIndex:index withObject:object]; 
-
-		/*NSString* object =  [symbols objectAtIndex:arc4random()%[symbols count]];
-	        
-		// проверка на совпадение старого и нового значения
-	        if (![[[self dnaArray] objectAtIndex:index] isEqualToString:object]) {
-		    // замена на новое значение
-	            [[self dnaArray] replaceObjectAtIndex:index withObject:object];
-	        } else {
-		    // если значения совпадают - вызываем текущий метод (рекурсию)
-		    // 100/capacity - процент для одного цикла
-	            [self mutate:100/capacity];
-	        }
-                */             
-		
-	}
+		// index -  случайный индекс
+        int index = arc4random()%capacity;
+        
+        while ([unique containsObject:[NSNumber numberWithInt:index]]) {
+            index = arc4random()%capacity;
+        }
+        
+        [unique addObject:[NSNumber numberWithInt: index]];
+    }
+        
+    for (NSNumber* rand in unique) {
+        NSString* object = [self differentObjectsAtIndex:[rand intValue]];
+        
+        [[self dnaArray] replaceObjectAtIndex:[rand intValue] withObject:object];
+    }
+    
+    [unique release];
 }
 
 int main (int argc, const char * argv[]) {
