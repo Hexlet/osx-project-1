5e2fdac80a2298a028a2352ba77ee65c1b2ea533
Yura Tolstik <yltsrc@gmail.com>
2012-10-31 14:48:41 +0300

    Initial Commit

---
+++ dna/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  dna
+//
+//  Created by Yura Tolstik on 10/31/12.
+//  Copyright (c) 2012 Yura Tolstik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
