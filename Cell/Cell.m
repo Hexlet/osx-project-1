49b5af385c3298ec0357859e71d5cb135a821b98
Georgiy Mostolovitsa <hello@gfranco.ru>
2012-11-01 03:15:38 +0400

    init

---
+++ Cell/Cell.m
@@ -0,0 +1,99 @@
+//
+//  Cell.m
+//  DNA
+//
+//  Created by Georgiy Mostolovitsa on 31.10.12.
+//  Copyright (c) 2012 Georgiy Mostolovitsa. All rights reserved.
+//
+
+#import "Cell.h"
+
+@implementation Cell
+
+
+// Создаёт и возвращает объект класса Cell с дефолтным
+// количеством нуклеотидов
+- (id)init {
+    
+    self = [super init];
+    
+    if (self)
+        [self fillCellWithCapacity:100];
+    
+    return self;
+    
+}
+
+
+// Создаёт и возвращает объект класса Cell с произвольным
+// количеством нуклеотидов
+- (id)initWithCapacity:(int)numItems {
+    
+    self = [super init];
+    
+    if (self)
+        [self fillCellWithCapacity:numItems];
+    
+    return self;
+    
+}
+
+
+// Возвращает случайный нуклеотид
+- (id)getRandomNucleotide {
+    return [_nucleotides objectAtIndex:arc4random() % _nucleotides.count];
+}
+
+
+// Заполняет ДНК переданным количеством нуклеотидов
+- (void)fillCellWithCapacity:(int)numItems {
+    
+    // Массив с возможными видами нуклеотидов. По-хорошему,
+    // это наверное должна быть константа, но я понял как её сделать
+    _nucleotides = [[NSArray alloc] initWithObjects:@"A", @"G", @"C", @"T", nil];
+    
+    // Инициализация ДНК
+    DNA = [[NSMutableArray alloc] init];
+    
+    // Переданное количество раз
+    for(int index = 0; index < numItems; index++)
+        
+        // Добавляем в ДНК нуклеотид
+        [DNA addObject:[self getRandomNucleotide]];
+    
+}
+
+
+// Считает количество различий в цепочках ДНК
+- (int)hammingDistance:(Cell *)anotherCell {
+
+    int count = 0; // Переменная для хранения найденных совпадений
+    
+    // Проходимся по нашей цепочке текущего инстанса
+    for (int index = 0; index < DNA.count; index++)
+        
+        // Проверяем, что в переданном объекте нуклеотидов не меньше чем у нас
+        if (index < [[anotherCell getDNA] count])
+            
+            // Сравниваем нуклеотиды
+            if ([[DNA objectAtIndex:index] isNotEqualTo:[[anotherCell getDNA] objectAtIndex:index]])
+                count = count + 1;
+    
+    
+    return count;
+    
+}
+
+
+// Геттер для ДНК
+- (id)getDNA {
+    return DNA;
+}
+
+
+// Метод позволяющий заменить определённый нуклеотид
+- (void)replaceAtIndex:(int)index withObject:(id)object {
+    [DNA replaceObjectAtIndex:index withObject:object];
+}
+
+@end
---
+++ DNA/main.m
@@ -0,0 +1,80 @@
+//
+//  main.m
+//  DNA
+//
+//  Created by Georgiy Mostolovitsa on 31.10.12.
+//  Copyright (c) 2012 Georgiy Mostolovitsa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Cell.h"
+
+@interface Cell (mutator)
+- (void)mutate:(int)percent;
+- (void)mutateWith:(int)percent;
+@end
+
+@implementation Cell (mutator)
+
+// Мутирует ДНК на переданное количество процентов
+- (void)mutate:(int)percent {
+
+    // Считаем, сколько элементов необходимо заменить
+    int mutateCount = (double)percent / [DNA count] * 100;
+    
+    // «Случайный массив» — массив для хранения случайных, неповторяющихся чисел
+    NSMutableArray *randomNumbers = [[NSMutableArray alloc] init];
+    
+    // Пока массив не заполнен до нужного количества
+    while (randomNumbers.count < mutateCount) {
+        
+        // Генерим случайное число которое должно быть от 0 до DNS.count - 1
+        NSNumber *random = [NSNumber numberWithInt:arc4random() % [DNA count]];
+        
+        // И если оно ешё не содержится в нашем массиве
+        if (![randomNumbers containsObject:random])
+            
+            // Добавляем его туда
+            [randomNumbers addObject:random];
+
+
+    }
+    
+    // Проходимся по случайному массиву
+    for (int index = 0; index < mutateCount; index++) {
+        
+        // Сохраняем в переменную позицию нуклеотида
+        // который должен мутировать и заменяем его случайным
+        int replaceIndex =[[randomNumbers objectAtIndex:index] intValue];
+        [self replaceAtIndex:replaceIndex withObject:[self getRandomNucleotide]];
+        
+    }
+    
+}
+
+// Синоним для mutate:
+- (void)mutateWith:(int)percent {
+    [self mutate:percent];
+}
+
+@end
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        Cell* cell1 = [[Cell alloc] init];
+        Cell* cell2 = [[Cell alloc] initWithCapacity:100];  // Вот так ещё можно
+        
+        NSLog(@"%d", [cell1 hammingDistance:cell2]);
+
+        [cell1 mutate:34];
+        [cell2 mutateWith:67];                              // И вот так ещё
+        
+        NSLog(@"%d", [cell1 hammingDistance:cell2]);
+        
+        
+    }
+    return 0;
+}
