113b9db09fafd507955416115c0a89cc7aa8ff5f
Taras Lyapun <taraslyapun@gmail.com>
2012-10-31 18:19:31 +0200

    created project

---
+++ osx-project-1/osx-project-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  osx-project-1
+//
+//  Created by Taras Lyapun on 31.10.12.
+//  Copyright (c) 2012 Taras Lyapun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
