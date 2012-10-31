22ff3a5a4ecc0770379e16aa0d8f97dff91e5a8c
skorochkin <skorochkin@gmail.com>
2012-10-31 23:41:52 +0200

    myDna project added

---
+++ myDna/myDna/Cell.h
@@ -0,0 +1,24 @@
+//
+//  Cell.h
+//  myDna
+//
+//  Created by Sergii on 10/31/12.
+//  Copyright (c) 2012 Sergii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cell : NSObject
+{
+    NSMutableArray *DNA;
+}
+
+-(id) init;
+
+-(NSString*) getRandom;
+-(NSMutableArray*) getDNA;
+-(int) hammingDistance: (NSMutableArray*) otherDNA;
+
+-(void) printDNA;
+
+@end
