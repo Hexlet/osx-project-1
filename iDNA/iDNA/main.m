f970523c5a0b779d4a736c5cd565d7e12e9dd9e6
Ivo Dimitrov <darwinium@gmail.com>
2012-11-01 22:58:27 +0400

    Создан пустой проект

---
+++ iDNA/iDNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iDNA
+//
+//  Created by Darwin on 01.11.12.
+//  Copyright (c) 2012 Ivo Dimitrov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
