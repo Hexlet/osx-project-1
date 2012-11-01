da520b6b78f02595ccbf82f9fe581a0d3fbd18c9
Sergey Starukhin <sergey@Mac-mini.local>
2012-11-01 23:39:09 +0400

    Initial commit

---
+++ DNA project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNA project
+//
+//  Created by Sergey Starukhin on 01.11.12.
+//  Copyright (c) 2012 Sergey Starukhin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
