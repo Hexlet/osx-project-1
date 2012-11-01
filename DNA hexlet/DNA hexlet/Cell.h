ac6a21c019fd102515b6f4d3543254a98be7c6fa
Eugene <jekahy@gmail.com>
2012-11-01 15:11:40 +0200

    project 1- DNA

---
+++ DNA hexlet/DNA hexlet/Cell.h
@@ -0,0 +1,13 @@
+//
+//  Cell.h
+//  DNA hexlet
+//
+//  Created by Eugene on 01.11.12.
+//  Copyright (c) 2012 Eugene. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cell : NSObject
+
+@end
---
+++ DNA hexlet/DNA hexlet/Cell.m
@@ -0,0 +1,13 @@
+//
+//  Cell.m
+//  DNA hexlet
+//
+//  Created by Eugene on 01.11.12.
+//  Copyright (c) 2012 Eugene. All rights reserved.
+//
+
+#import "Cell.h"
+
+@implementation Cell
+
+@end
