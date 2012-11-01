9f04d9ef1a8cb6ab47cb3ffa3d4388f6b99f92c4
Alexey Kulik <alexey.kulik@hotmail.com>
2012-11-01 23:51:29 +0300

    Changed company name

---
+++ osx-project-1/Cell+Mut.m
@@ -0,0 +1,13 @@
+//
+//  Cell+Mut.m
+//  osx-project-1
+//
+//  Created by Alexey Kulik on 11/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Cell+Mut.h"
+
+@implementation Cell (Mut)
+
+@end
