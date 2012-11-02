de0bce24ddd53a7ca10cf8f8b6f5e01f6fcc1575
Alexander Madyankin <alexander@madyankin.name>
2012-11-02 09:15:05 +0600

    Initial commit

---
+++ DNA/DNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNA
+//
+//  Created by Alexander Madyankin on 02.11.12.
+//  Copyright (c) 2012 Alexander Madyankin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
