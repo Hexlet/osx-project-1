3e6103da83561efd38b64eef62f7a36aa195c732
pustserg <psakaskif@gmail.com>
2012-11-02 22:31:35 +0400

    finished

--- project_OSX1/project_OSX1/Cell.m
+++ project_OSX1/project_OSX1/Cell.m
@@ -8,6 +8,7 @@
 
 #import "Cell.h"
 
+
 @implementation Cell
 
 -(id)init{
--- project_OSX1/project_OSX1/main.m
+++ project_OSX1/project_OSX1/main.m
@@ -7,6 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Cell.h>
+
+@interface Cell (mutator)
+-(void) mutate: int
+@end
+
+@implementation Cell (mutator)
+
+-(void) mutate: (int) X{
+    NSArray *tempArray = [NSArray arrayWithObjects: @'A', @'T', @'C', @'G', nil];
+    int maxStep = floor(100/XPercent);
+    for (int i = 0; i < 100; i = i + arc4random() % maxStep) {
+        int randomIndex = (arc4random() % 2) * (arc4random() % 2);
+        [DNA replaceObjectAtIndex:i withObject:[tempArray objectAtIndex:randomIndex]];
+    }
+}
+
+@end
 
 int main(int argc, const char * argv[])
 {
