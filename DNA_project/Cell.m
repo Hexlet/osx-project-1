4b9679a734d0c48336ba442e9717c0fd7798ea91
Sergey Pershenkov <swante@ya.ru>
2012-11-02 13:06:52 +0400

    02

--- DNA_project/Cell.m
+++ DNA_project/Cell.m
@@ -17,12 +17,12 @@
   
   if(self)
   {
-    _dna = [NSMutableArray new];
-    _dict = [NSArray arrayWithObjects:@"A", @"T", @"G", @"C", nil];
+    self.dna = [NSMutableArray new];
+    self.dict = [NSArray arrayWithObjects:@"A", @"T", @"G", @"C", nil];
     
     // Заполнение DNA случайными значениями из словаря
     for(int i = 0; i < DNA_LENGTH; ++i)
-      [_dna addObject: [_dict objectAtIndex: arc4random() % 4]];
+      [self.dna addObject: [self.dict objectAtIndex: arc4random() % 4]];
   }
   
   return self;
@@ -35,7 +35,7 @@
   
   for(int i = 0; i < DNA_LENGTH; ++i)
   {
-    if([[_dna objectAtIndex:i] isNotEqualTo:[c.dna objectAtIndex:i]])
+    if([[self.dna objectAtIndex:i] isNotEqualTo:[c.dna objectAtIndex:i]])
       ++hDistance;
   }
   
@@ -49,17 +49,22 @@
 // Замена X процентов символов в DNA в случайном порядке
 -(void)mutate:(int)xPercents
 {
-  int  x = (DNA_LENGTH/100.0) * xPercents; // Пересчитываем проценты в целое число символов
-  int  changedSymbolsCount = 0;            // Счетчик измененных символов
-  BOOL symbolChanged[DNA_LENGTH];          // Флаги изменения символов
+  // Ограничение диапазона xPercents
+  if(xPercents <= 0)
+    return;
+  else if(xPercents > 100)
+    xPercents = 100;
+  
+  long x = lround(DNA_LENGTH * 0.01 * xPercents); // Пересчитываем проценты в целое число символов
+  int  changedSymbolsCount = 0;                   // Счетчик измененных символов
+  BOOL symbolChanged[DNA_LENGTH];                 // Флаги изменения символов
   
   // Сброс всех флагов изменения
   for(int i = 0; i < DNA_LENGTH; ++i)
     symbolChanged[i] = NO;
   
   // Пока не будет изменено X символов
-  // При этом X не может быть больше длины DNA или отрицательным
-  while(changedSymbolsCount < x && changedSymbolsCount < DNA_LENGTH)
+  while(changedSymbolsCount < x)
   {
     int dnaIndex = arc4random() % DNA_LENGTH; // Случайный индекс в DNA
     
@@ -69,10 +74,10 @@
       int dictIndex = arc4random() % 4; // Случайный индекс в словаре
       
       // Если символ из словаря не совпадает с символом из DNA
-      if([[_dna objectAtIndex:dnaIndex] isNotEqualTo:[_dict objectAtIndex:dictIndex]])
+      if([[self.dna objectAtIndex:dnaIndex] isNotEqualTo:[self.dict objectAtIndex:dictIndex]])
       {
         // Меняем символ в DNA
-        [_dna replaceObjectAtIndex:dnaIndex withObject:[_dict objectAtIndex:dictIndex]];
+        [self.dna replaceObjectAtIndex:dnaIndex withObject:[self.dict objectAtIndex:dictIndex]];
         symbolChanged[dnaIndex] = YES; // Меняем флаг изменения символа
         ++changedSymbolsCount; // Увеличиваем число измененных символов
       }
