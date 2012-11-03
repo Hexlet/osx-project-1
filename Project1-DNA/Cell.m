bafaaa4db9c8eb4064b06f1593ef24e644613bea
Bogdan Pankiv <bogdan@fabrica.net.ua>
2012-11-04 00:18:41 +0200

    Project created

---
+++ Project1-DNA/Cell.m
@@ -0,0 +1,60 @@
+//
+//  Cell.m
+//  Project1-DNA
+//
+//  Created by Bogdan Pankiv on 11/3/12.
+//  Copyright (c) 2012 Bogdan Pankiv. All rights reserved.
+//
+
+#import "Cell.h"
+
+@implementation Cell
+
+-(id) init
+{
+    self = [super init];
+    
+    srandomdev();
+    
+    dna = [[NSMutableArray alloc] init];
+    for (int i=0; i<100; ++i)
+        [dna addObject: [[NSNumber alloc] initWithInt: random()%4] ];
+    
+    return self;
+}
+
+-(id) initWithCell:(Cell *)cell
+{
+    self = [super init];
+    dna = [[NSMutableArray alloc] init];
+    for (int i=0; i<100; ++i)
+        [dna addObject: [cell getDnaAtPosition:i] ];
+    return self;
+}
+
+-(int) hammingDistance:(Cell *)cell
+{
+    int distance = 0;
+    for (int i=0; i<100; ++i) {
+        if ([self getDnaAtPosition:i] != [cell getDnaAtPosition:i]) ++distance;
+    }
+    return distance;
+}
+
+-(NSNumber *) getDnaAtPosition:(int)pos
+{
+    if (pos<0 || pos>99) return NULL;
+    return dna[pos];
+}
+
+-(void) print
+{
+    NSArray *values = [NSArray arrayWithObjects:@"A", @"T", @"G", @"C", nil];
+    NSString *result = [[NSString alloc] init];
+    
+    for (int i=0; i<100; ++i)
+        result = [result stringByAppendingString: values[ [dna[i] intValue] ] ];
+
+    NSLog(@"%@", result);
+}
+@end
