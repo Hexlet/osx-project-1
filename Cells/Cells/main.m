5caac28672ac6f001d20bd59c77248b298c38fc4
Vladimir Korshunov <vldm@krsh.me>
2012-11-04 20:54:52 +0400

    Init with gitignore

---
+++ Cells/Cells/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Cells
+//
+//  Created by Vladimir Korshunov on 11/4/12.
+//  Copyright (c) 2012 Vladimir Korshunov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
