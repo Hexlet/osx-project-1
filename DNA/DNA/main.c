39ab7b1e16d1788ff7d07646de589edef7226c0a
Konstantin Kalbazov <kalbazov@gmail.com>
2012-11-05 01:30:56 +0400

    Initial commit
    Initial commit of project files

---
+++ DNA/DNA/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  DNA
+//
+//  Created by Kote on 11/5/12.
+//  Copyright (c) 2012 Kote. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
