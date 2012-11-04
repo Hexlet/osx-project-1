13cabe09f2d2fcedccb5e7ee133cd885f7cb0076
Alexandr <alex@Macbook.local>
2012-11-04 16:48:54 +0300

    Initial Commit

---
+++ osx-project-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  osx-project-1
+//
+//  Created by Alexandr on 11/4/12.
+//  Copyright (c) 2012 Alexandr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
