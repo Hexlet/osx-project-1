2c8db6db38720c50e0474b22895b320bc1a0430a
deksden <deksden@deksden.com>
2012-11-05 22:43:21 +0700

    My DNA project (c) deksden

---
+++ EfxDNA/EfxDNA/Source/Cell+Mutator.h
@@ -0,0 +1,14 @@
+//
+//  EfxDNA Sample project
+//  Cell category (mutator)
+//  (c) 2012, deksden.com
+//
+
+
+#import "Cell.h"
+
+@interface Cell (Mutator)
+
+-(void) mutate:(int) percentOfMutation;
+
+@end
---
+++ EfxDNA/EfxDNA/Source/Cell.h
@@ -0,0 +1,33 @@
+//
+//  EfxDNA Sample project
+//  Cell class
+//  (c) 2012, deksden.com
+//
+#import <Foundation/Foundation.h>
+
+@interface Cell : NSObject
+
+// my class can init array of any size
+@property int DNACapacity;
+@property int DNAVariations; // currently only default value "3" is supported!
+
+
+-(id) init;
+-(id) initWithCapacity: (int) aCapacity; // for debug: ability create small array of DNA
+// for future: (id) initWitCapacity: (int) aCapacity andVariations: (int) aVariations;
+
+-(int) hammingDistance:(Cell *) cell;
+
+// utility methods to support workflow without disclousure of DNA instance:
+-(NSString*) getDNAAtIndex:(int) aIndex; // return string representation of 0-based DNA index from array: [A,T,G,C]
+-(NSUInteger) getDNACount;
+-(NSString*) getDNAByIndex: (int) aIndex; // return string object at index
+-(void) replaceDNAAtIndex:(int) aIndex withValue: (NSString*) newValue; // replace object at index (for mutation)
+
+#ifdef EFXDEBUG
+
+@property NSString *logPrefix; // logging prefix
+-(void)logDNA; // log all DNA items
+#endif
+
+@end
