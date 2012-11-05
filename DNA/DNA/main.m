c646c6443059048263afe5853366b17921f9588d
Eugene Myslovets <infinity.gomel@gmail.com>
2012-11-05 14:51:01 +0200

    Initial commit

---
+++ DNA/DNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNA
+//
+//  Created by Mac on 11/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
