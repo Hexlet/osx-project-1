a6b80272f60fc22723f0b3870511ddd9871036e8
Maxim Zhukov <crmaxx@gmail.com>
2012-11-05 14:20:24 +0400

    Added empty console project.

---
+++ osx-project-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  osx-project-1
+//
+//  Created by Maxim Zhukov on 05.11.12.
+//  Copyright (c) 2012 Maxim Zhukov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
