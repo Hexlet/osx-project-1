b45b46d3c35f11719a43a6dce86f545fc1d75ff6
Roman N <terafloo@gmail.com>
2012-11-05 17:20:44 +0400

    FINAL

---
+++ Project-1/Cell+mutator.h
@@ -0,0 +1,15 @@
+//
+//  Cell+mutator.h
+//  Project-1
+//
+//  Created by Roman Nekhoroshev on 05.11.12.
+//  Copyright (c) 2012 Roman Nekhoroshev. All rights reserved.
+//
+
+#import "Cell.h"
+
+@interface Cell (mutator)
+
+-(void) mutate:(int)mutationPercentage;
+
+@end
