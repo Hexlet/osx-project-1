e3125ca398a1f8b2cf3776d56bf84fc41743a54d
orangeudav <orangeudav@gmail.com>
2012-11-05 17:23:43 +0400

    DNA project

---
+++ dna/Nucleotide.m
@@ -0,0 +1,25 @@
+//
+//  Nucleotide.m
+//  dna
+//
+//  Created by Anton Osenenko on 11/5/12.
+//  Copyright (c) 2012 Anton Osenenko. All rights reserved.
+//
+
+#import "Nucleotide.h"
+
+@implementation Nucleotide
+
+- (Nucleotide *)initRandom {
+    self = [super init];
+    
+    _nucleotideCode = (NucleotideCode)(arc4random() % (int)C);
+    
+    return self;
+}
+
+- (BOOL)isEqualToNucleotide:(Nucleotide *)nucleotide {
+    return _nucleotideCode == nucleotide.nucleotideCode;
+}
+
+@end
