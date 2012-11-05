cc93f8da3906f0487aa90ac7fc9e1e0402a434a2
iSergius <ron.rcf@gmail.com>
2012-11-05 17:01:07 +0600

    Correct method mutate

--- osx-project-1/osx-project-1/Mutator.m
+++ osx-project-1/osx-project-1/Mutator.m
@@ -13,23 +13,34 @@
 - (void) mutate:(int)m {
     
     NSMutableArray *prc = [NSMutableArray arrayWithCapacity:m];
+    NSNumber *num = NULL;
+    //int count = 0;
+    //int count2 = 0;
+    //int count3 = 0;
     
     for (int i = 0;i<m; i++) {
-        
-        NSNumber *src = [NSNumber numberWithUnsignedLong:arc4random() % 100];
-        NSNumber *dst = [NSNumber numberWithUnsignedLong:arc4random() % 100];
+      //  count2++;
+        NSNumber *src = [NSNumber numberWithUnsignedInt: arc4random() % 100];
+        NSNumber *dst = [NSNumber numberWithUnsignedInt: arc4random() % 100];
         
         if ([prc containsObject:dst]) {
+            i--;
+            //count++;
             continue;
         }
+        //count3++;
+        NSString *str = [DNA objectAtIndex:[src unsignedIntegerValue]];
         
-        NSString *str = [DNA objectAtIndex:[src unsignedLongValue]];
-        
-        [DNA replaceObjectAtIndex:[dst unsignedLongValue] withObject:str];
+        [DNA replaceObjectAtIndex:[dst unsignedIntegerValue] withObject:str];
         
         [prc addObject:dst];
-        
+        //NSLog(@"%u",[dst intValue]);
     }
+    for (int i = 0 ; i<m; i++) {
+        num = [prc objectAtIndex:i];
+        //NSLog(@"%d",[num intValue]);
+    }
+    //NSLog(@"%d %d %d",count,count2,count3);
 }
 @end
 
