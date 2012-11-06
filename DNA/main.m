4c4189fb03454c183e7d0d0d3e5be5bede908e65
Mac User <macuser@Mac-Users-iMac.local>
2012-11-06 20:50:53 +0200

    Initial Commit

---
+++ DNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNA
+//
+//  Created by Mac User on 11/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
