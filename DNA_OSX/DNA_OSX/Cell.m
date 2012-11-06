8c5bf1708c8440cf2be076991437989f07f68f8d
aquaxp <aquaxpi@gmail.com>
2012-11-06 21:54:11 +0700

    new mutation alg

--- DNA_OSX/DNA_OSX/Cell.m
+++ DNA_OSX/DNA_OSX/Cell.m
@@ -15,19 +15,16 @@
     if (self) {
         _DNA = [[NSMutableArray alloc] initWithCapacity:100]; // init DNA array
         NSString *protein[4] = {@"A",@"T",@"G",@"C"}; // temporary array hold proteins name
-        for (int i = 0; i < 100; i++) {
-            _DNA[i] = protein[(arc4random() % 4)]; // generating protins chain
-        }
+        
+        for (int i = 0; i < 100; i++) _DNA[i] = protein[(arc4random() % 4)]; // generating protins chain
     }
     return self;
 }
+// counting distance
 -(int)hammingDistance:(Cell *) cll{
     int hamDist = 0;
-    for (int i = 0; i < 100; i++) { // position2position comparsion
-        if(_DNA[i] != cll.DNA[i]){
-            hamDist++;
-        }
-    }
+    
+    for (int i = 0; i < 100; i++) if(_DNA[i] != cll.DNA[i]) hamDist++; // position2position comparsion
     return hamDist;
 }
 @end
--- DNA_OSX/DNA_OSX/main.m
+++ DNA_OSX/DNA_OSX/main.m
@@ -16,44 +16,33 @@
 @implementation Cell(mutator)
 -(void)mutate:(int) count{
     int changes = 0 ;
-    NSInteger position[100];
+    int position[100];
     NSString *protein[4] = {@"A",@"T",@"G",@"C"};
     
-    // black magic bycicle for generating unique random mutation position
-    for(NSInteger i = 0; i < 100; i++){
-        position[i] = -1;
-    }
+    for(int i = 0; i < 100; i++) position[i] = 0; // generating map of DNA changes 
+    
     while (changes < count) {
         int x = arc4random() % 100;
-        for (int i = 0; i < count; i++) { // check for uniqueness
-            if (position[i] == x){
-                continue;
-            }
-            else{
-                position[changes] = x;
-                changes++;
-                break;
-            }
+        
+        if(position[x] != 0) continue; // if changed, trying next random number
+        else{ // else change protein
+            self.DNA[x] = protein[(arc4random() % 4)];
+            position[x] += 1;
+            changes++;
         }
-    }
-    for(int i = 0; i < count; i++){ // applying changes on DNA array
-        self.DNA[position[i]] = protein[(arc4random() % 4)];
     }
 }
 @end
 
-int main(int argc, const char * argv[])
-{
-
+int main(int argc, const char * argv[]){
     @autoreleasepool {
         Cell *cell0, *cell1;
         cell0 = [[Cell alloc]init];
         cell1 = [[Cell alloc]init];
         NSLog(@"%i", [cell0 hammingDistance:cell1]);
-        [cell0 mutate:12];
-        [cell1 mutate:12];
+        [cell0 mutate:45];
+        [cell1 mutate:45];
         NSLog(@"%i", [cell0 hammingDistance:cell1]);
-        
     }
     return 0;
 }
