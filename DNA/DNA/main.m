29f592aef70beee40ddfdaf03b4362055d11a437
Anton Platonov <platosha@gmail.com>
2012-11-07 03:30:52 +0400

    new project

---
+++ DNA/DNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNA
+//
+//  Created by Anton Platonov on 7.11.12.
+//  Copyright (c) 2012 Anton Platonov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
