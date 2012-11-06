3ed91fa21bda39e7520e1fc77cce3004d726cbdc
Roman Dolgopolov <roman@sefus.ru>
2012-11-06 23:34:10 +0400

    may be slow but it works. I think ...

---
+++ osx-project-1/Cell.m
@@ -0,0 +1,55 @@
+//
+//  Cell.m
+//  osx-project-1
+//
+//  Created by Roman Dolgopolov on 06.11.12.
+//  Copyright (c) 2012 Roman Dolgopolov. All rights reserved.
+//
+
+#import "Cell.h"
+
+@implementation Cell
+
+-(id) init{
+    
+    self = [super init];
+    
+    if (self) {
+        
+        _dnaCapacity = 10;
+        
+        _DNA = [NSMutableArray arrayWithCapacity:_dnaCapacity];
+
+        _Nucleobases = [NSArray arrayWithObjects: @"A", @"T", @"G", @"C", nil];
+        
+        for (int i = 0; i < _dnaCapacity; i++) {
+
+            NSUInteger randomIndex = arc4random() % [_Nucleobases count];
+
+            [_DNA addObject:[_Nucleobases objectAtIndex:randomIndex]];
+            
+        }
+        
+        //NSLog(@"Init values: %@", _DNA);
+    }
+    
+    return self;
+}
+
+
+-(int) hammingDistance:(Cell*) cell{
+    
+    int distance = 0;
+    
+    for (int i = 0; i < [_DNA count]; i++) {
+        
+        if ([self.DNA objectAtIndex:i] != [cell.DNA objectAtIndex:i]) {
+            distance++;
+        }
+        
+    }
+    
+    return distance;
+}
+
+@end
