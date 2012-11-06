4389b8404b9669dbf964d219233dfb869acfb899
lilumi <lilumi@li.ru>
2012-11-07 00:53:17 +0200

    FINAL
    улучшил алгоритм формирования случайных индексов для лучшего чтения
    кода и оптимизации памяти

--- Cell+Mutator.m
+++ Cell+Mutator.m
@@ -11,7 +11,7 @@
 @implementation Cell 
 
 - (id)init {
-   if ( self= [super init]) {
+   if ( self = [super init]) {
       self.DNA = [NSMutableArray arrayWithCapacity:100];
        for (int i=0; i<100; i++) {
           [self.DNA insertObject:[self randomCharacter] atIndex:i];
@@ -23,7 +23,7 @@
 }
 
 - (id)initWithLenght:(int)length {
-    if ( self= [super init]) {
+    if ( self = [super init]) {
         if (length<=0) length=1; //проверка на нулевое и отрицательные значения длины ДНК
             self.DNA = [NSMutableArray arrayWithCapacity:length];
             for (int i=0; i<length; i++) {
@@ -78,34 +78,30 @@
     
     if (0<=p && p<=100) {
     
-    int n = round([self.DNA count] * p/100), length=1, randomInt;
+    int n = round([self.DNA count] * p/100); //вычисляем кол-во елементов, что нужно будеть поменять в ДНК
     NSInteger indexArray[n], i=0;
     //NSLog(@"длина днк: %ld, нужно поменять %i",[self.DNA count], n);
-    
     if (n>=1) {
         
-    bool $present= false;
-    randomInt = arc4random()%[self.DNA count];
-    while (i<length && length<=n) {
-        for (int j=0; j<length-1; j++) {
-            if (randomInt == indexArray[j]) {
-                $present = true;
-                break;
-            } else $present = false;
-        }
+        bool exists=false;
         
-        if ($present == true) randomInt = arc4random()%n; else {
+        do { // формируем массив случайных индексов
+            int randomInt = arc4random()%[self.DNA count];
             
-            indexArray[i] = randomInt;
-            //NSLog(@"Будем мутировать на позиции %ld", indexArray[i]);
-            i++;
-            length++;
-            $present = false;
-            randomInt = arc4random()%[self.DNA count];
-        }
-        
+            for (int j=0; j<i;j++){
+                if (indexArray[j]==randomInt) {
+                    exists=true; break; //если индекс уже есть в масиве, то нет смысла уже дальше искать.
+                }
+                else exists=false;
+            }
             
-    }
+            if (!exists) {
+                indexArray[i]=randomInt;
+                i++;
+            }
+            
+        } while (i<n); // пока не сформируем масив с нужным кол-вом индексов
+
         for (i=0; i<n; i++) {
             [self.DNA replaceObjectAtIndex:indexArray[i] withObject:[self replaceCharacter:[self.DNA objectAtIndex:indexArray[i] ] ]];
         }
@@ -115,9 +111,7 @@
     
   } else NSLog(@"Значение процентов не может быть больше 100%% или меньше нуля");
 
-    
-
-    
+        
 }
 
 @end
