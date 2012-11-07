9360ec0c506c546b3f6f0cfe2ae1a2d6241ea129
Sergey Kondratyev <ron.rcf@gmail.com>
2012-11-08 02:11:29 +0600

    Add check value percent in method mutate

--- osx-project-1/osx-project-1/Mutator.m
+++ osx-project-1/osx-project-1/Mutator.m
@@ -12,37 +12,42 @@
 
 - (void) mutate:(int)percent {
     
-    NSMutableArray *arrayMutableMembersDna = [NSMutableArray arrayWithCapacity:percent];
-    //NSNumber *num = NULL;
-    /*int count = 0;
-    int count2 = 0;
-    int count3 = 0;
-    int count4 = 0;*/
+    if ((0 < percent) && ( 100 >= percent )) {
     
-    for (int i = 0;i<percent; i++) {
-        //count2++;
-        NSNumber *choseRandomMemberDna = [NSNumber numberWithUnsignedInt: arc4random() % 100];
-        NSNumber *mutateMemberDna = [NSNumber numberWithUnsignedInt: arc4random() % 100];
+        NSMutableArray *arrayMutableMembersDna = [NSMutableArray arrayWithCapacity:percent];
+        //NSNumber *num = NULL;
+        /*int count = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;*/
         
-        if ([arrayMutableMembersDna containsObject:mutateMemberDna]) {
-            i--;
-        //    count++;
-            continue;
+        for (int i = 0;i<percent; i++) {
+            //count2++;
+            NSNumber *choseRandomMemberDna = [NSNumber numberWithUnsignedInt: arc4random() % 100];
+            NSNumber *mutateMemberDna = [NSNumber numberWithUnsignedInt: arc4random() % 100];
+            
+            if ([arrayMutableMembersDna containsObject:mutateMemberDna]) {
+                i--;
+            //    count++;
+                continue;
+            }
+            
+            NSString *memberDna = [DNA objectAtIndex:[choseRandomMemberDna unsignedIntegerValue]];
+            NSString *givenMemberDna = [DNA objectAtIndex:[mutateMemberDna unsignedIntegerValue]];
+            
+            if (memberDna == givenMemberDna) {
+                i--;
+            //    count4++;
+                continue;
+            }
+            //count3++;
+            [DNA replaceObjectAtIndex:[mutateMemberDna unsignedIntegerValue] withObject:memberDna];
+            
+            [arrayMutableMembersDna addObject:mutateMemberDna];
+            //NSLog(@"%u",[dst intValue]);
         }
-        
-        NSString *memberDna = [DNA objectAtIndex:[choseRandomMemberDna unsignedIntegerValue]];
-        NSString *givenMemberDna = [DNA objectAtIndex:[mutateMemberDna unsignedIntegerValue]];
-        
-        if (memberDna == givenMemberDna) {
-            i--;
-        //    count4++;
-            continue;
-        }
-        //count3++;
-        [DNA replaceObjectAtIndex:[mutateMemberDna unsignedIntegerValue] withObject:memberDna];
-        
-        [arrayMutableMembersDna addObject:mutateMemberDna];
-        //NSLog(@"%u",[dst intValue]);
+    } else {
+        NSLog(@"Value percent define over 100");
     }
     /*for (int i = 0 ; i<m; i++) {
         num = [arrayMutableMembersDna objectAtIndex:i];
