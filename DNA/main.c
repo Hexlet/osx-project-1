6f401bcb8cb12684921716c2eb06a892ca2cc327
Yury Krikun <yury.krikun@gmail.com>
2012-11-08 00:34:23 +0400

    init

---
+++ DNA/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  DNA
+//
+//  Created by sx on 08.11.12.
+//  Copyright (c) 2012 sx. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
