ad7e4664a602a9efd31f80b7a7723f936dda82ba
mvmn <sauron.inbox@gmail.com>
2012-11-08 17:36:45 +0200

    Memory management optimizations - use "free" after "malloc". Store DNA codes in static array.

--- HexletObjCTask01/HexletObjCTask01/Cell+mutation.m
+++ HexletObjCTask01/HexletObjCTask01/Cell+mutation.m
@@ -11,39 +11,37 @@
 
 @implementation Cell (mutation)
 -(void)mutate:(int)percent {
-    @autoreleasepool {
-        if(percent<1) return;
-        percent = percent%100;
-        if(percent==0) percent = 100;
-        // Yeah, I assume 100 length for DNA 'cause I'm too lazy to handle other cases
-        int* indexes = malloc(sizeof(int)*percent);
-        indexes[0] = rand()%100;
-        int length = 1;
-        while(length<percent) {
-            unsigned int nextIdx = arc4random()%100;
-            bool present = false;
-            for(int f=0; f<length; f++) {
-                if(indexes[f]==nextIdx) {
-                    present = true;
-                    break;
-                }
-            }
-            if(!present) {
-                indexes[length]=nextIdx;
-                length++;
+    if(percent<1) return;
+    percent = percent%100;
+    if(percent==0) percent = 100;
+    // Yeah, I assume 100 length for DNA 'cause I'm too lazy to handle other cases
+    int* indexes = malloc(sizeof(int)*percent);
+    indexes[0] = rand()%100;
+    int length = 1;
+    while(length<percent) {
+        unsigned int nextIdx = arc4random()%100;
+        bool present = false;
+        for(int f=0; f<length; f++) {
+            if(indexes[f]==nextIdx) {
+                present = true;
+                break;
             }
         }
-        //printf("Mutating at %d indexes\n", length);
-        for(int f=0; f<length; f++) {
-            //printf(" - index: %d\n", indexes[f]);
-            [self changeCell:self toRandomAt:indexes[f]];
+        if(!present) {
+            indexes[length]=nextIdx;
+            length++;
         }
     }
+    //printf("Mutating at %d indexes\n", length);
+    for(int f=0; f<length; f++) {
+        //printf(" - index: %d\n", indexes[f]);
+        [self changeCell:self toRandomAt:indexes[f]];
+    }
+    free(indexes);    
 }
 
-
+const char* DNA_CHAR_SET = "ATCG";
 -(void)changeCell:(Cell*)cell toRandomAt:(int)index {
-    char* DNA_CHAR_SET = "ATCG";
     unsigned int random = arc4random();
     //    [DNA replaceObjectAtIndex:index withObject:[NSString stringWithFormat:@"%c" , ' ']];
     const char currentVal = [Cell getAsCharFromDna:[cell getDna] atIndex:index];
