0cebe3bc6edb44faf6299b253ba27d76d5e761aa
savenko <sovahome85@gmail.com>
2012-11-08 16:13:01 +0400

    Замена на &&

--- DNA/DNA/Cell+Mutator.m
+++ DNA/DNA/Cell+Mutator.m
@@ -32,7 +32,7 @@
         
         //Следим чтобы ячейки в старом массиве не совпадали с новым и за тем что Ячейка не совпадала с тем чем будем менять
         
-        if([[oldDNA objectAtIndex:numCell] isEqual: [self.dna objectAtIndex:numCell]] || [oldDNA objectAtIndex:numCell]!=newCell){
+        if([[oldDNA objectAtIndex:numCell] isEqual: [self.dna objectAtIndex:numCell]] && [oldDNA objectAtIndex:numCell]!=newCell){
             [self.dna replaceObjectAtIndex:numCell withObject:newCell];
             i++;
         }
