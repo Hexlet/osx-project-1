ce6c43a88156b262a5cd4f8f944a579a5bffebbc
kipelovets <kipelovets@mail.ru>
2012-11-09 00:42:57 +0400

    done

---
+++ Cell+mutator.h
@@ -0,0 +1,15 @@
+//
+//  Cell+mutator.h
+//  DNA
+//
+//  Created by phantom on 08.11.12.
+//
+//
+
+#import "Cell.h"
+
+@interface Cell (mutator)
+
+- (void) mutate :(int)percent;
+
+@end
--- DNA/main.m
+++ DNA/main.m
@@ -7,15 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <stdlib.h>
+
+#import "Cell.h"
+#import "Cell+mutator.h"
 
 int main(int argc, const char * argv[])
 {
+    srandomdev();
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Cell * cell1 = [[Cell alloc] init];
+        Cell * cell2 = [[Cell alloc] init];
         
+        NSLog(@"cell1: %@", [cell1 toString]);
+        NSLog(@"cell2: %@", [cell2 toString]);
+        
+        NSLog(@"distance: %ld", [cell1 hammingDistance:cell2]);
+        
+        [cell1 mutate:70];
+        
+        NSLog(@"cell1 after mutation: %@", [cell1 toString]);
+        
+        NSLog(@"distance after mutation: %ld", [cell1 hammingDistance:cell2]);
     }
     return 0;
 }
