5a82a9d246c05cad24ac80d6dcf8b42a86f38e7a
Dmitry Zavorokhin <goodman116@gmail.com>
2012-11-09 02:52:47 +0400

    [f] it seems like previous commit was incomplete

--- goodman116-dna/goodman116-dna/Cell+Mutator.h
+++ goodman116-dna/goodman116-dna/Cell+Mutator.h
@@ -2,12 +2,14 @@
 //  Cell+Mutator.h
 //  goodman116-dna
 //
-//  Created by Dmitriy Zavorokhin on 11/6/12.
+//  Created by goodman116 on 11/6/12.
 //  Copyright (c) 2012 goodman116. All rights reserved.
 //
 
 #import "Cell.h"
 
 @interface Cell (Mutator)
+
+-(void) mutate:(int)percentageOfMutation;
 
 @end
--- goodman116-dna/goodman116-dna/main.m
+++ goodman116-dna/goodman116-dna/main.m
@@ -2,19 +2,30 @@
 //  main.m
 //  goodman116-dna
 //
-//  Created by Dmitriy Zavorokhin on 11/5/12.
+//  Created by goodman116 on 11/5/12.
 //  Copyright (c) 2012 goodman116. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import "Cell.h"
+#import "Cell+Mutator.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Cell *cell1 = [[Cell alloc] init];
+        Cell *cell2 = [[Cell alloc] init];
+//        Cell *cell3 = [cell2 clone];
+        
+        NSLog(@"Hamming distance before mutation is: %i", [cell1 hammingDistance:cell2]);
+        
+        [cell1 mutate:15];
+        [cell2 mutate:100];
+
+//        NSLog(@"Check if this value is %i when cell2 has 100%% mutation: %i", size, [cell2 hammingDistance:cell3]);
+        NSLog(@"Hamming distance after mutation is: %i", [cell1 hammingDistance:cell2]);
         
     }
     return 0;
