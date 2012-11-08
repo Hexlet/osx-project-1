4577747e6dd7b538710c185df8163df87e11f2a6
kipelovets <kipelovets@mail.ru>
2012-11-08 22:05:59 +0400

    Initial Commit

---
+++ DNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNA
+//
+//  Created by phantom on 08.11.12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
