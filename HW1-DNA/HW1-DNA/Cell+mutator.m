aaac46018c4739cd6acbe7802e3ae11449b4251e
Victor <gafiatulin@gmail.com>
2012-11-08 22:12:28 +0400

    remove redundant copying

--- HW1-DNA/HW1-DNA/Cell+mutator.m
+++ HW1-DNA/HW1-DNA/Cell+mutator.m
@@ -13,15 +13,14 @@
 -(void)mutate: (int)mutationRate
 {
     NSAssert((mutationRate >= 0) && (mutationRate <= 100), @"MutationRate ∉ [0,100]");
-    static NSMutableArray *intArr = nil;
-    if(!intArr){
-        intArr = [[NSMutableArray alloc] initWithCapacity:capacity];
+    static NSMutableArray *arrShuffle = nil;
+    if(!arrShuffle){
+        arrShuffle = [[NSMutableArray alloc] initWithCapacity:capacity];
         for(int i = 0; i < capacity; i++)
         {
-            [intArr addObject: [NSNumber numberWithInt: i]];
+            [arrShuffle addObject: [NSNumber numberWithInt: i]];
         }
     }
-    NSMutableArray *arrShuffle = [intArr mutableCopy];
     [arrShuffle shuffle];
     for (int i = 0; i< capacity*mutationRate/100; i++)
     {
