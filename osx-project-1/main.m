7537dfb576848ee8bb0abc850c75a93d5b06e14d
Aizat Mustafin <aizat.mustafin@gmail.com>
2012-11-10 00:23:51 +0400

    Junk removed

--- osx-project-1/main.m
+++ osx-project-1/main.m
@@ -9,22 +9,6 @@
 #import <Foundation/Foundation.h>
 #import "Cell.h"
 
-@interface NSMutableArray (Shuffling)
-- (void)shuffle;
-@end
-
-@implementation NSMutableArray (Shuffling)
-- (void)shuffle
-{
-    NSUInteger count = [self count];
-    for (NSUInteger i = 0; i < count; ++i) {
-        // Select a random element between i and end of array to swap with.
-        NSInteger nElements = count - i;
-        NSInteger n = (arc4random() % nElements) + i;
-        [self exchangeObjectAtIndex:i withObjectAtIndex:n];
-    }
-}
-@end
 
 @interface Cell (Mutator)
 -(void)mutate:(int)percentsToChange;
