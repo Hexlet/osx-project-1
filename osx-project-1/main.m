5a29cce5b457da945968a3ff7255cd10c7e94a3b
Aizat Mustafin <aizat.mustafin@gmail.com>
2012-11-09 15:15:00 +0300

    Initial Commit

---
+++ osx-project-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  osx-project-1
+//
+//  Created by Aizat on 11/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
