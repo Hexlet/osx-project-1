1312ef1fd89058ad0c96fcde0bc73013a47b42d7
Yuriy Ostapyuk <zimne@ukr.net>
2012-11-09 09:53:03 +0200

    Some small fixes

--- dna/dna/main.m
+++ dna/dna/main.m
@@ -24,21 +24,28 @@
         // array to store already processed indexes
         NSMutableArray *indexes = [[NSMutableArray alloc] init];
         
-        for (int i = 0; i < percent; i++) {
+        for (int i = 0; i < percent;) {
             // getting random index of nucliotide
             index = [NSNumber numberWithInteger:arc4random()%100];
-
+            
             // if current nucliotide was not changed yet
             if (![indexes containsObject: index]) {
-                 
+                
+                // changing nucliotide in dna structure
+                NSString *newNucleotide;
+
+                // searching for not equal new nucleotide
+                do {
+                    newNucleotide = [Cell getNucliotideFromArray];
+                } while ([self.dna[[index intValue]] isEqualTo: newNucleotide]);
+
+                self.dna[[index intValue]] = newNucleotide;
+                
                 // storing index of changed nucliotide
                 [indexes addObject:index];
+                
+                i++;
             }
-        }
-        
-        // changing nucliotide in dna structure
-        for(NSNumber *i in indexes) {            
-            self.dna[[i intValue]] = [Cell getNucliotideFromArray];
         }
     }
 
