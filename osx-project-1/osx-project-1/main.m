07e3a2d3188587d1cdc339ab7d6f890ade67bfa8
Sergey <smoke1@mail.ru>
2012-11-11 00:03:12 +0400

    Проект создан

---
+++ osx-project-1/osx-project-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  osx-project-1
+//
+//  Created by smoke on 11.11.12.
+//  Copyright (c) 2012 smoke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
