2ede714da061be12bf11319c45f53e3058108634
alexflom <alexflom@ukr.net>
2012-11-10 12:31:45 +0200

    *

--- osx/osx/main.m
+++ osx/osx/main.m
@@ -6,17 +6,29 @@
 //  Copyright (c) 2012 flom. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
+#import "Cell.h"
+#import "mutator.h"
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Cell *cell1 = [[Cell alloc] init];
+        Cell *cell2 = [[Cell alloc] init];
+        [cell1 print];
+        [cell2 print];
+        NSLog(@"distance=%i", [cell1 hammingDistace:cell2]);
+        
+        [cell1 mutate:30];
+        
+        [cell1 print];
+        [cell2 print];
+        NSLog(@"distance=%i", [cell1 hammingDistace:cell2]);
+        
         
     }
     return 0;
 }
-
