9a4298c87b44df766aed9c9c3408deefc99e460f
Sergey Nebolsin <nebolsin@gmail.com>
2012-11-10 16:44:47 +0530

    Create Cell class

---
+++ DNAProcessor/Cell.m
@@ -0,0 +1,13 @@
+//
+//  Cell.m
+//  DNAProcessor
+//
+//  Created by Sergey Nebolsin on 10.11.12.
+//  Copyright (c) 2012 Sergey Nebolsin. All rights reserved.
+//
+
+#import "Cell.h"
+
+@implementation Cell
+
+@end
---
+++ DNAProcessor/DNAProcessor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DNAProcessor
+//
+//  Created by Sergey Nebolsin on 10.11.12.
+//  Copyright (c) 2012 Sergey Nebolsin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+    }
+    return 0;
+}
+
