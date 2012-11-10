76f7fb61dac11fe617f6164ef59b316bb71153e2
alexflom <alexflom@ukr.net>
2012-11-10 12:23:44 +0200

    initial

---
+++ osx/osx/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  osx
+//
+//  Created by flom on 10.11.12.
+//  Copyright (c) 2012 flom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
