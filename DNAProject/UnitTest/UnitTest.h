f10ca8a901be7f88f089d17b9d6eacc4d5badd18
Vitaliy <vith77@gmail.com>
2012-11-11 19:36:52 +0600

    Исправления

---
+++ DNAProject/UnitTest/UnitTest.h
@@ -0,0 +1,13 @@
+//
+//  UnitTest.h
+//  UnitTest
+//
+//  Created by VITALIY NESTERENKO on 06.11.12.
+//  Copyright (c) 2012 VITALIY NESTERENKO. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTest : SenTestCase
+
+@end
---
+++ DNAProject/UnitTest/UnitTest.m
@@ -0,0 +1,34 @@
+//
+//  UnitTest.m
+//  UnitTest
+//
+//  Created by VITALIY NESTERENKO on 06.11.12.
+//  Copyright (c) 2012 VITALIY NESTERENKO. All rights reserved.
+//
+
+#import "UnitTest.h"
+#import "main.h"
+
+@implementation UnitTest
+
+- (void)setUp
+{
+    [super setUp];
+  
+     Cell *cell =[[Cell alloc] init];    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+  
+}
+
+@end
