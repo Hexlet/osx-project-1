2f11afbaf60b67cf6e9235dd8edae0333bad5628
Alexey Popov <alexey@fresh-flash.ru>
2012-11-11 21:53:41 +0400

    Initial Commit

---
+++ DNK/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNK
+//
+//  Created by Alexey Popov on 11.11.12.
+//  Copyright (c) 2012 Alexey Popov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
