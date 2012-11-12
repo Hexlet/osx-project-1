a4d0e64b29fc4d99fa4485275f69867d3460cb10
witzawitz <witzawitz@gmail.com>
2012-11-12 20:29:29 +0400

    Initial commit.

---
+++ DNA/DNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNA
+//
+//  Created by n on 12.11.12.
+//  Copyright (c) 2012 n. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
