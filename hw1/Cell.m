a4e7f76a9a680181b0ab249926976e5d9ab84d43
sneezeman <499thz@gmail.com>
2012-11-13 03:33:47 +0400

    Final
    Final!

---
+++ hw1/Cell.m
@@ -0,0 +1,50 @@
+//
+//  Cell.m
+//  hw1
+//
+//  Created by admin on 11/12/12.
+//  Copyright (c) 2012 admin. All rights reserved.
+//
+
+#import "Cell.h"
+
+@implementation Cell
+
+int sizeArray = 100;
+
+-(id) init {
+    self = [super init];
+    if (self) {
+        
+        
+        _types = [NSMutableArray arrayWithObjects:@"A", @"T", @"G", @"C", nil];
+        _DNA = [NSMutableArray arrayWithCapacity:100];
+        
+        for (sizeArray = 0; sizeArray < 100; sizeArray++) {
+            [_DNA addObject:_types[arc4random() %4]];
+        }
+    }
+    return self;
+}
+
+
+-(int) hammingDistance:(Cell *)cell {
+    int hamming = 0;
+    
+    for (sizeArray = 0; sizeArray < 100; sizeArray++) {
+        if (_DNA [sizeArray] != cell.DNA[sizeArray]) {
+            hamming++;
+        }
+    }
+    return hamming;
+}
+
+- (NSString *) findGenNot:(NSString *)current {
+    NSString *gen = [[NSString alloc] init];
+    while (gen != current){
+        gen = self.types[arc4random() % 4];
+    }
+    return gen;
+}
+
+@end
---
+++ hw1/Mutate.m
@@ -0,0 +1,37 @@
+//
+//  Mutate.m
+//  hw1
+//
+//  Created by admin on 11/13/12.
+//  Copyright (c) 2012 admin. All rights reserved.
+//
+
+#import "Mutate.h"
+
+@implementation Cell (mutate)
+
+- (void) mutate: (int)percent {
+    if(percent > 100){
+        percent = 100;
+    }
+    
+    int i;
+    
+    NSMutableArray *changedPositions = [[NSMutableArray alloc] init];
+    NSString *index, *gen;
+    for(i = 0; i < percent; i++){
+        index = [NSString stringWithFormat: @"%d", arc4random() % percent];
+        if(changedPositions.count <= 0){
+            [changedPositions addObject: index];
+        } else if([changedPositions indexOfObject:index] > changedPositions.count){
+            [changedPositions addObject: index];
+        } else {
+            i--;
+        }
+    }
+    for (i = 0; i < changedPositions.count; i++) {
+        gen = [self findGenNot:[self.DNA objectAtIndex:[changedPositions[i] integerValue]]];
+        [self.DNA insertObject:gen atIndex: [changedPositions[i] integerValue]];
+    }
+}
+@end
