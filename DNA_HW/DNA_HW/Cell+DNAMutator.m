d560f081ffe172e72c0c7338258b32ab9de17a6f
Anatoly Tarnavsky <anatolyt@gmail.com>
2012-11-12 17:26:15 +0200

    Исправлен mutator алгоритм
    Была ошибка в mutator категории в результате которой была возможна
    ситуация когда одна и таже ячейка в DNA менялась несколько раз,
    исправлено!

--- DNA_HW/DNA_HW/Cell+DNAMutator.m
+++ DNA_HW/DNA_HW/Cell+DNAMutator.m
@@ -15,10 +15,15 @@
     
     int letter2Change;      //serial number of letter to change
     NSString *newLetter;
+    NSMutableArray *cellsAlreadyModified = [[NSMutableArray alloc]init]; //we'll store number of cell we've already modified for not modify it twice
     
     while (percentage != 0) //we continue while all required letters haven't changed
     {
         letter2Change = arc4random() % 100;
+        if ([cellsAlreadyModified indexOfObject:[NSNumber numberWithInt:letter2Change] ] != NSNotFound) {   //if it returns other than NSNotFound object it means we already changed this cell
+            continue;                                                                                       //and we skip this loop
+        }
+        [cellsAlreadyModified addObject:[NSNumber numberWithInt:letter2Change]];            //in other case we add this cell number to 'cellsAlreadyModified' array and performing change
         
         while ([[oldDNA objectAtIndex:letter2Change]isEqualToString:[self.DNA objectAtIndex:letter2Change]])  //if both letters are same it means we haven't change it yet
         {  
