20f1ae1dc9c9ca367640b896362dce0f4a71117d
Nik <thelastvarius@gmail.com>
2012-12-03 23:28:09 +0600

    CashCount

---
+++ CashCount/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  CashCount
+//
+//  Created by Nik on 02.12.12.
+//  Copyright (c) 2012 Nik. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ CashCount/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CashCount
+//
+//  Created by Nik on 02.12.12.
+//  Copyright (c) 2012 Nik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
