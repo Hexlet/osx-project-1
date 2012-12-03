e94f32501619c4e8cd84d2823d84b9c5be8760b2
Nik <thelastvarius@gmail.com>
2012-12-03 23:32:10 +0600

    CashCount

---
+++ CashCount/CashCount/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CashCount
+//
+//  Created by Nik on 02.12.12.
+//  Copyright (c) 2012 Nik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
