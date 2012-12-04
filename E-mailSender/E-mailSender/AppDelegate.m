2f1b8f0e9272dc03090000d5e2293db8c1cbc267
Fimasik <fimasik@gmail.com>
2012-12-04 04:57:10 +0300

    ng
    qw

---
+++ E-mailSender/E-mailSender/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  E-mailSender
+//
+//  Created by Дмитрий on 03.12.12.
+//  Copyright (c) 2012 Dmitrii. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ E-mailSender/E-mailSenderTests/E_mailSenderTests.h
@@ -0,0 +1,13 @@
+//
+//  E_mailSenderTests.h
+//  E-mailSenderTests
+//
+//  Created by Дмитрий on 03.12.12.
+//  Copyright (c) 2012 Dmitrii. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface E_mailSenderTests : SenTestCase
+
+@end
