5c26b2dd83ec419c87dbec4dcfe9f3159e666821
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2013-01-03 02:23:45 +0200

    Initial Commit

---
+++ DNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNA
+//
+//  Created by Olexandr Skrypnyk on 03.01.13.
+//  Copyright (c) 2013 Olexandr Skrypnyk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
