8d673100939ca74b932b021404f2f8f85915c61a
Alexander Pronin <pronin.alx@gmail.com>
2013-01-07 21:18:07 +0200

    Test commit

---
+++ aproDNA/aproDNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  aproDNA
+//
+//  Created by apro on 07.01.13.
+//  Copyright (c) 2013 apro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
